63e8877afd908dd15505cd98efb6a0a8a2fa25e1
Jay Kim <macuser@imac.lan>
2019-12-06 22:01:42 -0500

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ console.log(path.join(__dirname, '../public'))
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -139,6 +140,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000 , () => {
-    console.log('server is up on port 3000')
+app.listen(port , () => {
+    console.log('server is up on port ' + port) 
 })
